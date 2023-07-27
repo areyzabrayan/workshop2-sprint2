c4bab9c88aa2aac2032c78a42f0f0688607c5819
sergiotechx <sergiotechx@yahoo.com>
2023-07-27 12:48:09 -0500

    Plantilla con router 2

--- src/components/pages/technology/technology.jsx
+++ src/components/pages/technology/technology.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {  useParams } from "react-router-dom";
 
 const Technology = () => {
+  const { id } = useParams();
   return (
-    <div>Nuevo</div>
+    <div>Nuevo {id}</div>
 
   )
 }
