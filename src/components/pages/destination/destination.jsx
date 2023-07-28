a21837c5cc20cdcabfa0f76fb1f7d9750fedc893
Alexis Garcia <alexis25yo@gmail.com>
2023-07-28 17:24:36 -0500

    add section destination

--- src/components/pages/destination/destination.jsx
+++ src/components/pages/destination/destination.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import {  useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./destination.scss";
+import imagen from "../../../assets/destination/image-moon.webp";
+import { destination } from "../../../data/dataDestination";
 
 const Destination = () => {
-  const { id } = useParams();
-  return (
-    <div>Destination {id}</div>
-  )
-}
+  const [info, setInfo] = useState(destination[0]);
 
-export default Destination
+  const handleButtonClick = (index) => {
+    setInfo(destination[index]);
+  };
+
+  return (
+    <div className="container">
+      <div className="details">
+        <figure>
+          <h2>
+            <span className="special-text">01</span> PICK YOUR DESTINATION
+          </h2>
+          <img src={info.images.png} />
+        </figure>
+        <section className="details__navButton">
+          <div className="details__buttons">
+            <button onClick={() => handleButtonClick(0)}>MOON</button>
+            <button onClick={() => handleButtonClick(1)}>MARS</button>
+            <button onClick={() => handleButtonClick(2)}>EUTORA</button>
+            <button onClick={() => handleButtonClick(3)}>TITAN</button>
+          </div>
+          <h1 className="details__title">{info.name}</h1>
+          <p className="details__description">{info.description}</p>
+          <div className="details__footer">
+            <section>
+              <span>avg. distance</span>
+              <h3>{info.distance}</h3>
+            </section>
+            <section>
+              <span>EST travel time</span>
+              <h3>{info.travel}</h3>
+            </section>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Destination;
