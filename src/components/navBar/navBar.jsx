672fe9884dfabe00b4e7360691895ebe459face4
Alexis Garcia <alexis25yo@gmail.com>
2023-07-30 10:25:33 -0500

    added navbar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.scss";
 
 const NavBar = () => {
-    return (
-        <div className='NavBar'>
-            <nav>
-                
-                    <span><Link to="/">Home</Link></span>
-                    <span><Link to="/destination/1">Destination</Link></span>
-                    <span><Link to="/crew/1">Crew</Link></span>
-                    <span><Link to="/technology">Technology</Link></span>
-                
-            </nav>
-        </div>
-    )
-}
+  return (
+    <div className="NavBar">
+      <nav>
+        <span>
+          <Link className="a" to="/">
+            Home
+          </Link>
+        </span>
+        <span>
+          <Link className="a" to="/destination/1">
+            Destination
+          </Link>
+        </span>
+        <span>
+          <Link className="a" to="/crew/1">
+            Crew
+          </Link>
+        </span>
+        <span>
+          <Link className="a" to="/technology">
+            Technology
+          </Link>
+        </span>
+      </nav>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Technology from '../components/pages/technology/technology'
-import Home from './../components/pages/home/home';
-import Destination from './../components/pages/destination/destination';
-import Crew from './../components/pages/crew/crew';
-import NavBar from '../components/navBar/navBar';
-
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Technology from "../components/pages/technology/technology";
+import Home from "./../components/pages/home/home";
+import Destination from "./../components/pages/destination/destination";
+import Crew from "./../components/pages/crew/crew";
+import NavBar from "../components/navBar/navBar";
+//import "./router.scss";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <NavBar/>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/destination/:id" element={<Destination />} />
-                <Route path="/crew/:id" element={<Crew />} />
-                <Route path="/technology" element={<Technology />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <div className="RouterContainer">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination/:id" element={<Destination />} />
+          <Route path="/crew/:id" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
