f1cad54e17dcf9077153fc8fe730e85529ec69aa
Brayan Areiza <areizabrayan11@gmail.com>
2023-07-30 10:38:19 -0500

    update responsiv

--- src/components/pages/technology/technology.jsx
+++ src/components/pages/technology/technology.jsx
@@ -1,41 +1,38 @@
-import React, { useEffect, useState } from "react";
-import "./technology.scss";
-import imageLaunchVehiclePortrait from "../../../assets/technology/image-launch-vehicle-portrait.jpg";
-import imageSpaceportPortrait from "../../../assets/technology/image-spaceport-portrait.jpg";
-import imageSpaceCapsulePortrait from "../../../assets/technology/image-space-capsule-portrait.jpg";
-import imageLaunchVehiclelandscape from "../../../assets/technology/image-launch-vehicle-landscape.jpg";
-import imageSpaceportlandscape from "../../../assets/technology/image-spaceport-landscape.jpg";
-import imageSpaceCapsulelandscape from "../../../assets/technology/image-space-capsule-landscape.jpg";
+import React, { useEffect, useState } from 'react';
+import './technology.scss';
+import imageLaunchVehiclePortrait from '../../../assets/technology/image-launch-vehicle-portrait.jpg';
+import imageSpaceportPortrait from '../../../assets/technology/image-spaceport-portrait.jpg';
+import imageSpaceCapsulePortrait from '../../../assets/technology/image-space-capsule-portrait.jpg';
+import imageLaunchVehiclelandscape from '../../../assets/technology/image-launch-vehicle-landscape.jpg';
+import imageSpaceportlandscape from '../../../assets/technology/image-spaceport-landscape.jpg';
+import imageSpaceCapsulelandscape from '../../../assets/technology/image-space-capsule-landscape.jpg';
 
 const Technology = () => {
   const infoTech = [
     {
-      name: "Launch vehicle",
-      images: {
-        portrait: imageLaunchVehiclePortrait,
-        landscape: imageLaunchVehiclelandscape,
+      "name": "Launch vehicle",
+      "images": {
+        "portrait": imageLaunchVehiclePortrait,
+        "landscape": imageLaunchVehiclelandscape,
       },
-      description:
-        "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+      "description": "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
     },
     {
-      name: "Spaceport",
-      images: {
-        portrait: imageSpaceportPortrait,
-        landscape: imageSpaceportlandscape,
+      "name": "Spaceport",
+      "images": {
+        "portrait": imageSpaceportPortrait,
+        "landscape": imageSpaceportlandscape,
       },
-      description:
-        "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+      "description": "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
     },
     {
-      name: "Space capsule",
-      images: {
-        portrait: imageSpaceCapsulePortrait,
-        landscape: imageSpaceCapsulelandscape,
+      "name": "Space capsule",
+      "images": {
+        "portrait": imageSpaceCapsulePortrait,
+        "landscape": imageSpaceCapsulelandscape,
       },
-      description:
-        "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-    },
+      "description": "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+    }
   ];
 
   const [currentTech, setCurrentTech] = useState(infoTech[0]);
@@ -50,12 +47,14 @@ const Technology = () => {
       setIsTable(window.innerWidth < 768);
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
+
 
     handleResize();
 
+   
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
@@ -66,30 +65,23 @@ const Technology = () => {
         <h2>SPACE LAUNCH 101</h2>
       </div>
       <article className="containerT__main">
-        <div className="containerT__info">
-          <div className="containerT__buttons">
-            {infoTech.map((tech, index) => (
-              <button key={index} onClick={() => handleButtonClick(index)}>
-                {index + 1}
-              </button>
-            ))}
-          </div>
-          <div className="containerT__text">
-            <h3>The terminology</h3>
-            <h1>{currentTech.name}</h1>
-            <p>{currentTech.description}</p>
-          </div>
+      <div className='containerT__info'>
+        <div className="containerT__buttons">
+          {infoTech.map((tech, index) => (
+            <button key={index} onClick={() => handleButtonClick(index)}>
+              {index + 1}
+            </button>
+          ))}
+        </div>
+        <div className="containerT__text">
+          <h3>The terminology</h3>
+          <h1>{currentTech.name}</h1>
+          <p>{currentTech.description}</p>
+        </div>
         </div>
         <div className="containerT__image">
           <figure>
-            <img
-              src={
-                isTable
-                  ? currentTech.images.landscape
-                  : currentTech.images.portrait
-              }
-              alt=""
-            />
+            <img src={isTable ? currentTech.images.landscape : currentTech.images.portrait} alt="" />
           </figure>
         </div>
       </article>
@@ -99,4 +91,7 @@ const Technology = () => {
 
 //update Now
 
+
 export default Technology;
+
+
