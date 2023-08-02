c49be4d950acc02b53abfe803b6093fea63fb285
Brayan Areiza <areizabrayan11@gmail.com>
2023-08-01 23:40:46 -0500

    update branch

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://areyzabrayan.github.io/workshop2-sprint2'
 })
